84ddad7e06a7b51423a74384c8390e63ca55bcb1
Sylvain <sylvain.denyse@gmail.com>
2018-11-06 15:40:40 +0100

    init hapi server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import { Server } from 'hapi'
+import { config } from './utils/config'
+
+export default function start() {
+  const server = new Server({
+    host: config.hapi.host,
+    port: config.hapi.port,
+    routes: { cors: { origin: ['*'] } }
+  })
+
+  server.start().then(
+    res => console.log('Server listening on', server.info.uri),
+    err => {
+      console.error(err)
+    }
+  )
+
+  return server
+}
---
+++ src/utils/config.js
@@ -0,0 +1,12 @@
+import env from 'common-env'
+
+export const config = env().getOrElseAll({
+  node: {
+    env: 'development'
+  },
+  hapi: {
+    host: 'localhost',
+    port: 4444
+  },
+  postgres: {}
+})
